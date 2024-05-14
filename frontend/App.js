a69dcaa79e6e4b2eef9bedb98ec2a3486ccea1d9
Jennifer Klang <klang.jennifer@gmail.com>
2024-05-14 09:23:29 +0200

    Add tabNavigation screens

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-export default function App() {
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import CalendarScreen from "./screens/CalendarScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ExerciseScreen from "./screens/ExerciseScreen";
+import styles from "./AppStyles";
+
+const homeName = "Home";
+const detailsName = "Details";
+const calendarName = "Calendar";
+const settingsName = "Settings";
+const exerciseName = "Exercise";
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hejjj Ponneee</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === detailsName) {
+              iconName = focused ? "list" : "list-outline";
+            } else if (rn === calendarName) {
+              iconName = focused ? "calendar" : "calendar-outline";
+            } else if (rn === settingsName) {
+              iconName = focused ? "settings" : "settings-outline";
+            } else if (rn === exerciseName) {
+              iconName = focused ? "barbell" : "barbell-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarStyle: styles.tabBar,
+          tabBarLabelStyle: styles.tabBarLabel,
+          tabBarActiveTintColor: "#467685",
+          tabBarInactiveTintColor: "grey",
+          headerShown: false,
+        })}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={exerciseName} component={ExerciseScreen} />
+        <Tab.Screen name={calendarName} component={CalendarScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+// import { StatusBar } from "expo-status-bar";
+// import { Text, View } from "react-native";
+// import styles from "./AppStyles";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hejjj Ponneee</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
---
+++ frontend/AppStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#E8EAED",
+  },
+  tabBar: {
+    backgroundColor: "white",
+    height: 70,
+  },
+  tabBarLabel: {
+    paddingBottom: 10,
+    fontSize: 10,
+    color: "grey",
+  },
+});
